3661d362110cace5d77d037f245127c3189a4a7f
alex <reizero@live.com>
2024-12-31 14:05:32 +0800

    add package

--- conf.d/alias.sh
+++ conf.d/alias.sh
@@ -3,4 +3,5 @@
 # Configure apm alias
 if ! is_configured 'apm=' /home/$user/.bashrc; then
   echo "alias apm=$path/setup.sh" | sudo tee -a /home/$user/.bashrc >/dev/null
+  echo "eval "$(thefuck --alias f)"" | sudo tee -a /home/$user/.bashrc >/dev/null
 fi
