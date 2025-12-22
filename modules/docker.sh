bd5e9168f0082046ecfe80785cfde3abcc5d21f4
alex <reizero@live.com>
2025-12-22 22:59:12 +0800

    feat(hyprland): swap mako for swaync + full docker daemon
    - enable IPv6, logging, jsdelivr mirror
    - add obsidian, color-picker, maximize binds
    - bring Bibata cursors, notification widget

--- modules/docker.sh
+++ modules/docker.sh
@@ -9,13 +9,28 @@ if [ ! -d /etc/docker/ ]; then
 fi
 
 # 国内加速都关停
-# if [ ! -f /etc/docker/daemon.json ] || [ -z "`cat /etc/docker/daemon.json | grep registry-mirrors`" ]; then
-# echo '{
-#   "registry-mirrors": [
-#     "https://dockerproxy.com"
-#   ]
-# }' | sudo tee /etc/docker/daemon.json
-# fi
+if [ ! -f /etc/docker/daemon.json ] || [ -z "`cat /etc/docker/daemon.json | grep registry-mirrors`" ]; then
+echo '{
+  "experimental": true,
+  "fixed-cidr-v6": "fd00::/80",
+  "group": "docker",
+  "hosts": [
+    "fd://"
+  ],
+  "ip6tables": true,
+  "ipv6": true,
+  "live-restore": false,
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-file": "3",
+    "max-size": "10m"
+  },
+  "registry-mirrors": [
+    "https://dockercf.jsdelivr.fyi"
+  ],
+  "userland-proxy": false
+}' | sudo tee /etc/docker/daemon.json
+fi
 
 if [ ! -f /etc/systemd/system/docker.service.d/proxy.conf ]; then
 sudo mkdir -p /etc/systemd/system/docker.service.d/
